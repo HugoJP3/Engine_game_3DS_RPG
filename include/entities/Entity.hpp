9120636fc8359230fd34424d2a37cbc29c41a1a1
HugoJarrin <hjp3@alu.ua.es>
2026-03-31 18:21:55 +0200

    Modificado Entity.hpp para trabajar con tamaños lógicos y solo transformarlos al renderizar

--- include/entities/Entity.hpp
+++ include/entities/Entity.hpp
@@ -16,27 +16,26 @@ class Entity {
     protected:
         // General
         float x, y, z;
-        int width, height;
+        float width, height; // Tamaño lógico, sin escala
 
-        // Lógica colisiones
+        // Lógica colisiones, sin escala
         float colWidth, colHeight;
-        float offsetX = 0.0f;
-        float offsetY = 0.0f;
+        float offsetX = 0.0f, offsetY = 0.0f;
+
         bool solid = false;
 
         C2D_SpriteSheet spriteSheet;
+
         FlagManager* flagManager;
 
-        // Eliminar
-        bool pendingRemoval = false;
+        bool pendingRemoval = false; // Variable que le indica al manager que debe eliminar la instancia
 
-    
         void drawCollision(float camX, float camY) {
             u32 colorRect = C2D_Color32(0, 255, 0, 100);
             float debugZ = 0.8f;
             float thickness = 5.0f;
-            float drawX = x + offsetX - camX;
-            float drawY = y + offsetY - camY;
+            float drawX = (x + offsetX - camX) * Config::globalScale;
+            float drawY = (y + offsetY - camY) * Config::globalScale;
             float scaledSizeX = colWidth;
             float scaledSizeY = colHeight;
 
@@ -47,11 +46,16 @@ class Entity {
         } 
         
     public:
-        Entity(float x, float y, float z, int w, int h, float colW, float colH, FlagManager* flagManager)
+        Entity(float x, float y, float z,
+            float w, float h,
+            float colW, float colH,
+            FlagManager* flagManager)
             : x(x), y(y), z(z),
             width(w), height(h),
             colWidth(colW), colHeight(colH),
-            spriteSheet(nullptr), flagManager(flagManager), pendingRemoval(false) {}
+            offsetX(0), offsetY(0),
+            spriteSheet(nullptr), flagManager(flagManager),
+            pendingRemoval(false) {}
         
         virtual ~Entity() {}
 
@@ -60,12 +64,50 @@ class Entity {
         virtual void draw(float camX, float camY) = 0;
 
         virtual void onInteract(InteractionContext& ctx) = 0;
-        
-        void setSpriteSheet(C2D_SpriteSheet sheet) { this->spriteSheet = sheet; }
+
+        // --- INICIALIZACIÓN ---
+        void setSpriteSheet(C2D_SpriteSheet sheet) { spriteSheet = sheet; }
 
         void setSolid(bool s) { solid = s; }
         bool isSolid() const { return solid; }
 
+        // --- POSICIÓN ---
+        float getX() const { return x; }
+        float getY() const { return y; }
+
+        void setX(float newX) { x = newX; }
+        void setY(float newY) { y = newY; }
+
+        // --- TAMAÑO LÓGICO ---
+        float getWidth() const { return width; }
+        float getHeight() const { return height; }
+
+        // --- HITBOX ---
+        float getColWidth() const {return colWidth; }
+        float getColHeight() const {return colHeight; }
+
+        float getOffsetX() const { return offsetX; }
+        float getOffsetY() const { return offsetY; }
+
+        void setCollision(float w, float h, float offX, float offY) {
+            colWidth = w;
+            colHeight = h;
+            offsetX = offX;
+            offsetY = offY;
+        }
+
+        // --- CENTRO ---
+        float getCenterX() const { return x + width / 2.0f; }
+        float getCenterY() const { return y + height / 2.0f; }
+
+        // --- RENDER SPACE ---
+        float getRenderWidth() const { return width * Config::globalScale; }
+        float getRenderHeight() const { return height * Config::globalScale; }
+
+        float getRenderX(float camX) const { return (x - camX) * Config::globalScale; }
+        float getRenderY(float camY) const { return (y - camY) * Config::globalScale; }
+
+        // --- MÉTODO COLISIÓN ---
         bool checkCollision(Entity& other)
         {         
             float offset1X = offsetX;
@@ -87,21 +129,7 @@ class Entity {
             return (left1 < right2 && right1 > left2 && top1 < bottom2 && bottom1 > top2);
         }
 
-        float getX() const { return x; }
-        float getY() const { return y; }
-
-        void setX(float newX) { x = newX; }
-        void setY(float newY) { y = newY; }
-
-        float getWidth() const { return width; }
-        float getHeight() const { return height; }
-
-        float getColWidth() const {return colWidth; }
-        float getColHeight() const {return colHeight; }
-
-        float getOffsetX() const { return offsetX; }
-        float getOffsetY() const { return offsetY; }
-
+        // --- ESTADO A ELIMINAR ---
         bool isPendingRemoval() const { return pendingRemoval; }
         void setPendingRemoval(bool pr) { pendingRemoval = pr; }
 };
